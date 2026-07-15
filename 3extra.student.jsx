363b0ac91792c986fdd831801c7d88fa5a3a8d7a
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-15 13:24:48 +0530

    Create 3extra.student.jsx

---
+++ 3extra.student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>    
+      <p>Name : Suyash Karnale</p>
+      <p>Course : B.Sc.IT</p>
+      <p>Semester : V</p>
+    </div>
+  );
+}
+export default Student;
