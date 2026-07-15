a4ab934ffc6323b14b2307227fcd2161472f6aa7
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-15 13:22:24 +0530

    Create 3e.student.jsx

---
+++ 3e.student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Components</h2>    
+      <p>Name : Suyash Karnale</p>
+      <p>Course : MCA</p>
+      <p>Semester : III</p>
+    </div>
+  );
+}
+export default Student;
