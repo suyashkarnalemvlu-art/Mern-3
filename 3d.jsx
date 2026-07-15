6473a9caba98183bd801090765e6e7b23b4f3166
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-15 13:20:27 +0530

    Create 3d.jsx

---
+++ 3d.jsx
@@ -0,0 +1,28 @@
+.container {
+    width: 700px;
+    margin: 50px auto;
+    padding: 20px;
+    border: 2px solid black;
+    border-radius: 10px;
+    background-color: whitesmoke;
+    text-align: center;
+}
+h1 {
+    color: darkblue;
+}
+p {
+    font-size: 18px;
+    color: black;
+}
+button {
+    background-color: green;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+button:hover {
+    background-color: darkgreen;
+}
