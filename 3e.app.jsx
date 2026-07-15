c6bdf5db04f5f425a368f1c31961e9fef1d3d68f
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-15 13:21:59 +0530

    Create 3e.app.jsx

---
+++ 3e.app.jsx
@@ -0,0 +1,12 @@
+	import Student from "./components/Student";
+function App() {
+  return (
+    <div>
+      <h1>This is fuctional component practical</h1>
+      <hr />
+      <Student />
+    </div>
+  );
+}
+export default App;
+
