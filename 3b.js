4328f6621d9402a9a5b1c554697df64bb621802e
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-15 13:14:53 +0530

    Create 3b.js

---
+++ 3b.js
@@ -0,0 +1,16 @@
+function App()
+{
+  const emp ="Suyash Karnale";
+  const dept="IT";
+  const sal =50000;
+  return(
+    <div>
+      <h1>Employee Details :</h1>
+      <p>Name : {emp}</p>
+      <p>Department : {dept}</p>
+      <p>Salary : {sal}</p>
+      <h4>Suyash Karnale (T012)</h4>
+    </div>
+  );
+}
+export default App
